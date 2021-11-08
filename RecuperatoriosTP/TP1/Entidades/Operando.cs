737558756d3e42b99d0ff06f6d9b47eb046564cf
Caffaro237 <facundodarian3@gmail.com>
2021-11-08 00:22:43 -0300

    Empezando el recuperatorio del TP1

---
+++ RecuperatoriosTP/TP1/Entidades/Operando.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Operando
+    {
+        #region Atributos
+
+        private double numero;
+
+        #endregion
+
+        #region Constructores
+
+        public Operando()
+        {
+
+        }
+
+        public Operando(double numero)
+        {
+
+        }
+
+        public Operando(string srtNumero)
+        {
+
+        }
+
+        #endregion
+
+        #region Propiedades
+
+        /*public string Numero
+        {
+            set
+            {
+                this.numero = value;
+            }
+        }*/
+
+        #endregion
+
+        #region Metodos
+
+
+
+        #endregion
+
+        #region Sobrecarga de operadores
+
+        public static double operator +(Operando n1, Operando n2)
+        {
+            return n1.numero + n2.numero;
+        }
+        public static double operator -(Operando n1, Operando n2)
+        {
+            return n1.numero - n2.numero;
+        }
+        public static double operator /(Operando n1, Operando n2)
+        {
+            if(n2.numero == 0)
+            {
+                return double.MinValue;
+            }
+
+            return n1.numero / n2.numero;
+        }
+        public static double operator *(Operando n1, Operando n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        #endregion
+
+    }
+}
