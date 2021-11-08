0ce73d4f7df25204d1519daf7759faacb670f622
Caffaro237 <facundodarian3@gmail.com>
2021-11-08 01:03:49 -0300

    Finalizacion de los metodos de la clase Operando

--- RecuperatoriosTP/TP1/Entidades/Operando.cs
+++ RecuperatoriosTP/TP1/Entidades/Operando.cs
@@ -18,57 +18,116 @@ namespace Entidades
 
         public Operando()
         {
-
+            this.numero = 0;
         }
 
         public Operando(double numero)
         {
-
+            this.numero = numero;
         }
 
         public Operando(string srtNumero)
         {
-
+            this.Numero = srtNumero;
         }
 
         #endregion
 
         #region Propiedades
 
-        /*public string Numero
+        public string Numero
         {
             set
             {
-                this.numero = value;
+                this.numero = this.ValidarOperando(value);
             }
-        }*/
+        }
 
         #endregion
 
         #region Metodos
 
-        public double ValidarOperando(string strNumero)
+        private double ValidarOperando(string strNumero)
         {
+            double numero;
+
+            if(double.TryParse(strNumero, out numero))
+            {
+                return numero;
+            }
+
             return 0;
         }
 
         public string BinarioDecimal(string binario)
         {
-            return "";
+            if (this.EsBinario(binario))
+            {
+                return Convert.ToUInt64(binario, 2).ToString();
+            }
+
+            return "Valor invalido";
         }
 
         public string DecimalBinario(double numero)
         {
-            return "";
+            string retornoBinario = "";
+            UInt64 numeroNatural;
+
+            numeroNatural = (UInt64)Math.Abs(numero);
+
+            if (numeroNatural == 0)
+            {
+                retornoBinario = "0";
+            }
+
+            while (numeroNatural > 0)
+            {
+                if (numeroNatural % 2 == 0)
+                {
+                    retornoBinario = "0" + retornoBinario;
+                }
+                else
+                {
+                    retornoBinario = "1" + retornoBinario;
+                }
+
+                numeroNatural = numeroNatural / 2;
+            }
+
+            return retornoBinario;
         }
 
         public string DecimalBinario(string strNumero)
         {
-            return "";
+            string retorno = "Valor invalido";
+
+            if (double.TryParse(strNumero, out this.numero))
+            {
+                if (this.numero > 0)
+                {
+                    retorno = this.DecimalBinario(this.numero);
+                }
+            }
+
+            return retorno;
         }
 
         private bool EsBinario(string binario)
         {
+            if (binario.Length == 0)
+            {
+                return false;
+            }
+
+            foreach (char item in binario)
+            {
+                if (item != '0' && item != '1')
+                {
+                    return false;
+                }
+            }
+
             return true;
         }
 
